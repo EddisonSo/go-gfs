7a74f025228ea037c921c9bbb1acf1ee5a86f49f
EddisonSo <eddisonso@gmail.com>
2025-06-30 21:23:34 +0800

    added chunkserver.go for chunkserver internal logic

--- internal/chunkserver/chunkserver.go
+++ internal/chunkserver/chunkserver.go
@@ -1,1 +1,27 @@
 package chunkserver
+
+import (
+	"context"
+	pb "eddisonso.com/go-gfs/gen/filetransfer"
+)
+
+type FileServiceServer struct {
+	pb.UnimplementedFileServiceServer
+}
+
+func (s *FileServiceServer) UploadFile(ctx context.Context, req *pb.UploadFileRequest) (*pb.UploadFileResponse, error) {
+	// TODO: Implement upload logic
+	return &pb.UploadFileResponse{
+		Success:       true,
+		Transactionid: "TRANSACTION_ID",
+	}, nil
+}
+
+func (s *FileServiceServer) DownloadFile(ctx context.Context, req *pb.DownloadFileRequest) (*pb.DownloadFileResponse, error) {
+	// TODO: Implement download logic
+	return &pb.DownloadFileResponse{
+		Success:       true,
+		Transactionid: "TRANSACTION_ID",
+	}, nil
+}
+
