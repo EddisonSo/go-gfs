58d205e100f7a69da4fe117faaa5b4c5f89ea896
EddisonSo <eddisonso@gmail.com>
2025-06-30 21:23:16 +0800

    created main.go for chunkserver for chunkserver binary entrypoint

--- cmd/chunkserver/main.go
+++ cmd/chunkserver/main.go
@@ -1,6 +1,27 @@
 package main
 
-import "eddisonso.com/go-gfs/internal/chunkserver"
+import (
+	"log"
+	"net"
+
+	"google.golang.org/grpc"
+	pb "eddisonso.com/go-gfs/gen/filetransfer"
+	"eddisonso.com/go-gfs/internal/chunkserver"
+)
 
 func main() {
+	lis, err := net.Listen("tcp", ":50051")
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	grpcServer := grpc.NewServer()
+
+	pb.RegisterFileServiceServer(grpcServer, &chunkserver.FileServiceServer{})
+
+	log.Println("gRPC server listening on :50051")
+	if err := grpcServer.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+
 }
