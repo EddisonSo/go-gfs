033df62d09af57a335c875a8dae9245262ff2a8b
EddisonSo <eddisonso@gmail.com>
2025-08-19 15:44:44 -0400

    created transaction.go for defining transactions for uploading and downloading chunks between client-chunkserver and chunkserver-chunkserver

---
+++ internal/chunkserver/transaction.go
@@ -0,0 +1,53 @@
+package chunkserver
+
+const (
+	UPLOAD = iota
+	DOWNLOAD = iota
+)
+
+/*
+ * Transaction represents a file operation transaction in the chunk server.
+ * TransactionId: Unique identifier for the transaction.
+ * ChunkId: Identifier for the chunk being operated on.
+ * Operation: Type of operation being performed (UPLOAD or DOWNLOAD).
+ */
+type Transaction struct {
+	TransactionID string
+	ChunkID       string
+	Operation     int
+}
+
+
+/*
+ * Creates a new transaction with the given transaction ID, chunk ID, and operation type.
+ */
+func NewTransaction(transactionID string, chunkID string, operation int) *Transaction {
+	return &Transaction{
+		TransactionID: transactionID,
+		ChunkID:       chunkID,
+		Operation:     operation,
+	}
+}
+
+/*
+ * Returns the operation type of the transaction.
+ */
+func (t *Transaction) GetOperation() int {
+	return t.Operation
+}
+
+
+/*
+ * Returns the transaction ID of the transaction.
+ */
+func (t *Transaction) GetTransactionID() string {
+	return t.TransactionID
+}
+
+/*
+ * Returns the chunk ID of the transaction.
+ */
+func (t *Transaction) GetChunkID() string {
+	return t.ChunkID
+}
+
