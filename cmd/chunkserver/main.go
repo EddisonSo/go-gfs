bdac0d86a7b21c2e2bf0e1cd21a8ef1dfee5c9a6
eddison <eddisonso@gmail.com>
2025-08-23 01:49:49 -0400

    added cmd arguments

--- cmd/chunkserver/main.go
+++ cmd/chunkserver/main.go
@@ -2,16 +2,22 @@ package main
 
 import (
 	"fmt"
-
+	"flag"
 	"eddisonso.com/go-gfs/internal/chunkserver"
 	"eddisonso.com/go-gfs/internal/chunkserver/csstructs"
 )
 
 func main() {
 	fmt.Println("Starting Chunk Server...")
+	
+	port := flag.Int("p", 8080, "Port for the chunk server to listen on")
+	hostname := flag.String("h", "localhost", "Hostname for the chunk server")
+
+	flag.Parse()
+
 	config := csstructs.ChunkServerConfig{
-		Hostname: "localhost",
-		Port:     8080,
+		Hostname: *hostname, 
+		Port:     *port,
 		Id:       "chunkserver-1",
 		Dir:      "tmp/",
 	}
