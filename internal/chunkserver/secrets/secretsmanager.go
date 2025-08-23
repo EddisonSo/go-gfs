2ff7b467343e9ed6d6767cd9c6dd21ec6fc9dad4
eddison <eddisonso@gmail.com>
2025-08-23 00:53:55 -0400

    temp secrets service

--- internal/chunkserver/secrets/secretsmanager.go
+++ internal/chunkserver/secrets/secretsmanager.go
@@ -4,6 +4,10 @@ import (
 	"github.com/golang-jwt/jwt/v5"
 )
 
+/*
+ * Retrieves secrets for signing and verifying JWT
+ * Notice that it says "temporary secret", DO NOT USE IT FOR PRODUCTION
+ */
 func GetSecret(token *jwt.Token) (any, error) {
 	return []byte("temporary secret"), nil
 }
