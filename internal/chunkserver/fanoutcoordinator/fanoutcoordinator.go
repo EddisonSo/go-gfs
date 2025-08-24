247398d8b25143dc3e1606e4b81e87866b034683
eddison <eddisonso@gmail.com>
2025-08-24 02:00:57 -0400

    created fanoutcoordinator methods and struct

--- internal/chunkserver/fanoutcoordinator/fanoutcoordinator.go
+++ internal/chunkserver/fanoutcoordinator/fanoutcoordinator.go
@@ -1,7 +1,32 @@
 package fanoutcoordinator
 
+import (
+	"context"
+	"net"
+
+	"eddisonso.com/go-gfs/internal/chunkserver/csstructs"
+	"eddisonso.com/go-gfs/internal/chunkserver/stagedchunk"
+)
 
 
 type fanoutcoordinator struct {
-	primary 
+	conn net.Conn
+	replicas []csstructs.ReplicaIdentifier
+	stagedchunk *stagedchunk.StagedChunk
+}
+
+func NewFanoutCoordinator(conn net.Conn) *fanoutcoordinator {
+	return &fanoutcoordinator{conn: conn}
+}
+
+func (f *fanoutcoordinator) AddReplicas(replicas []csstructs.ReplicaIdentifier) {
+	f.replicas = append(f.replicas,)
+}
+
+func (f *fanoutcoordinator) SetStagedChunk(stagedchunk *stagedchunk.StagedChunk) {
+	f.stagedchunk = stagedchunk
+}
+
+func (f *fanoutcoordinator) StartFanout(ctx context.Context) error {
+	return nil
 }
