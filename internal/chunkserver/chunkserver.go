7490052c57596c3d65161e8b9d53faab5080e462
eddison <eddisonso@gmail.com>
2025-08-24 17:29:59 -0400

    changed chunkserver to handle clients instead on seperate goroutines (which require individual ports)

--- internal/chunkserver/chunkserver.go
+++ internal/chunkserver/chunkserver.go
@@ -1,6 +1,10 @@
 package chunkserver
 
 import (
+	"net"
+	"strconv"
+	"log/slog"
+	"encoding/binary"
 	"eddisonso.com/go-gfs/internal/chunkserver/downloader"
 	"eddisonso.com/go-gfs/internal/chunkserver/csstructs"
 )
@@ -17,6 +21,30 @@ func NewChunkServer(config csstructs.ChunkServerConfig) *ChunkServer {
 }
 
 func (cs *ChunkServer) Start() {
-	downloaderService := downloader.NewFileDownloadService(cs.config, 60)
-	downloaderService.ListenAndServe()
+	ln, err := net.Listen("tcp", cs.config.Hostname + ":" + strconv.Itoa(cs.config.Port))
+	if err != nil {
+		slog.Error("Failed to start listener", "address", cs.config.Hostname + ":" + strconv.Itoa(cs.config.Port), "error", err)
+	}
+
+	downloader, err:= downloader.NewFileDownloadService(cs.config, 30)
+	if err != nil {
+		slog.Error("Failed to create FileDownloadService", "error", err)
+		return
+	}
+
+	slog.Info("FileDownloadService is listening", "address", cs.config.Hostname + ":" + strconv.Itoa(cs.config.Port))
+	for {
+		conn, err := ln.Accept()
+		if err != nil {
+			slog.Error("Failed to accept connection", "error", err)
+			continue
+		}
+		buf := make([]byte, 4)
+		action := csstructs.Action(binary.BigEndian.Uint32(buf))
+		switch action {
+			case csstructs.Download:
+				go downloader.HandleDownload(conn)
+				
+		}
+	}
 }
