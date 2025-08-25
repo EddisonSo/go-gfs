0aa3a433e33754ad98dad336e0edf3b2ddab0b90
eddison <eddisonso@gmail.com>
2025-08-25 00:53:52 -0400

    updated to use new packages

--- internal/chunkserver/chunkserver.go
+++ internal/chunkserver/chunkserver.go
@@ -1,18 +1,14 @@
 package chunkserver
 
 import (
-	"net"
-	"strconv"
-	"log/slog"
-	"encoding/binary"
-	"eddisonso.com/go-gfs/internal/chunkserver/downloader"
 	"eddisonso.com/go-gfs/internal/chunkserver/csstructs"
+	"eddisonso.com/go-gfs/internal/chunkserver/dataplane"
+	"eddisonso.com/go-gfs/internal/chunkserver/replicationplane"
 )
 
 type ChunkServer struct {
 	config csstructs.ChunkServerConfig
 }
-
 
 func NewChunkServer(config csstructs.ChunkServerConfig) *ChunkServer {
 	return &ChunkServer{
@@ -21,30 +17,8 @@ func NewChunkServer(config csstructs.ChunkServerConfig) *ChunkServer {
 }
 
 func (cs *ChunkServer) Start() {
-	ln, err := net.Listen("tcp", cs.config.Hostname + ":" + strconv.Itoa(cs.config.Port))
-	if err != nil {
-		slog.Error("Failed to start listener", "address", cs.config.Hostname + ":" + strconv.Itoa(cs.config.Port), "error", err)
-	}
-
-	downloader, err:= downloader.NewFileDownloadService(cs.config, 30)
-	if err != nil {
-		slog.Error("Failed to create FileDownloadService", "error", err)
-		return
-	}
-
-	slog.Info("FileDownloadService is listening", "address", cs.config.Hostname + ":" + strconv.Itoa(cs.config.Port))
-	for {
-		conn, err := ln.Accept()
-		if err != nil {
-			slog.Error("Failed to accept connection", "error", err)
-			continue
-		}
-		buf := make([]byte, 4)
-		action := csstructs.Action(binary.BigEndian.Uint32(buf))
-		switch action {
-			case csstructs.Download:
-				go downloader.HandleDownload(conn)
-				
-		}
-	}
+	dataPlane := dataplane.NewDataPlane(cs.config)
+	replicationPlane := replicationplane.NewReplicationPlane(cs.config)
+	go dataPlane.Start()
+	replicationPlane.Start()
 }
