29e10bde5fbd19f10144af2e3cf2964af3c52a8a
eddison <eddisonso@gmail.com>
2025-08-25 00:51:54 -0400

    added dataplane

---
+++ internal/chunkserver/dataplane/dataplane.go
@@ -0,0 +1,57 @@
+package dataplane
+
+import (
+	"net"
+	"strconv"
+	"log/slog"
+	"encoding/binary"
+	"eddisonso.com/go-gfs/internal/chunkserver/downloader"
+	"eddisonso.com/go-gfs/internal/chunkserver/csstructs"
+)
+
+type DataPlane struct {
+	config csstructs.ChunkServerConfig
+}
+
+func NewDataPlane(config csstructs.ChunkServerConfig) *DataPlane {
+	return &DataPlane {
+		config: config,
+	}
+}
+
+func (cs *DataPlane) Start() {
+	ln, err := net.Listen("tcp", cs.config.Hostname + ":" + strconv.Itoa(cs.config.DataPort))
+	if err != nil {
+		slog.Error("Failed to start listener", "address", cs.config.Hostname + ":" + strconv.Itoa(cs.config.DataPort), "error", err)
+	}
+
+	downloader, err:= downloader.NewFileDownloadService(cs.config, 30)
+	if err != nil {
+		slog.Error("Failed to create FileDownloadService", "error", err)
+		return
+	}
+
+	slog.Info("FileDownloadService is listening", "address", cs.config.Hostname + ":" + strconv.Itoa(cs.config.DataPort))
+	for {
+		conn, err := ln.Accept()
+		if err != nil {
+			slog.Error("Failed to accept connection", "error", err)
+			continue
+		}
+		slog.Info("New connection established", "remote_addr", conn.RemoteAddr().String())
+
+		buf := make([]byte, 4)
+		n, err := conn.Read(buf)
+		if err != nil || n != 4 {
+			slog.Error("Failed to read action from connection", "error", err)
+			return
+		}
+
+		action := csstructs.Action(binary.BigEndian.Uint32(buf))
+		switch action {
+			case csstructs.Download:
+				go downloader.HandleDownload(conn)
+				
+		}
+	}
+}
