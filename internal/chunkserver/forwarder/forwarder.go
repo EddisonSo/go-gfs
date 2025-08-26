3499d5bb1f788da7bf0293662b11636eb717f3db
eddison <eddisonso@gmail.com>
2025-08-26 02:50:39 -0400

    modified to use dataport and replicaport

--- internal/chunkserver/forwarder/forwarder.go
+++ internal/chunkserver/forwarder/forwarder.go
@@ -4,6 +4,7 @@ import (
 	"io"
 	"log/slog"
 	"net"
+	"strconv"
 
 	"eddisonso.com/go-gfs/internal/chunkserver/csstructs"
 )
@@ -31,17 +32,17 @@ func NewForwarder(replica csstructs.ReplicaIdentifier, opId string, chunkHandle 
 
 func (f *Forwarder) StartForward() error {
 	slog.Info("Starting forwarder", "replica", f.replica.Hostname, "opId", f.OpId, "chunkHandle", f.chunkHandle)
-	replicaConn, err := net.Dial("tcp", f.replica.Hostname)
+	replicaConn, err := net.Dial("tcp", f.replica.Hostname + ":" + strconv.Itoa(f.replica.ReplicationPort))
 	if err != nil {
-		slog.Error("Failed to connect to replica", "replica", f.replica.Hostname, "error", err)
+		slog.Error("Failed to connect to replica", "replica", f.replica.Hostname + ":" + strconv.Itoa(f.replica.ReplicationPort),"error", err)
 		return err
 	}
 	defer replicaConn.Close()
-	slog.Info("Connected to replica", "replica", f.replica.Hostname)
+	slog.Info("Connected to replica", "replica", f.replica.Hostname + ":" + strconv.Itoa(f.replica.ReplicationPort))
 
 	_, err = io.Copy(replicaConn, f.Pr)
 	if err != nil {
-		slog.Error("Failed to forward data", "replica", f.replica.Hostname, "error", err)
+		slog.Error("Failed to forward data", "replica",  f.replica.Hostname + ":" + strconv.Itoa(f.replica.ReplicationPort),"error", err)
 		return err
 	}
 	return nil
