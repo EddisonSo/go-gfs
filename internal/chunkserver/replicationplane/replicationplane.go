54c7b10a619ef8dbc57a8ba053e125677bdaa45e
eddison <eddisonso@gmail.com>
2025-09-03 23:03:50 -0400

    changed to using streaming grpc

--- internal/chunkserver/replicationplane/replicationplane.go
+++ internal/chunkserver/replicationplane/replicationplane.go
@@ -1,19 +1,57 @@
 package replicationplane
 
 import (
-	"context"
+	"io"
+	"errors"
+
 	pb "eddisonso.com/go-gfs/gen/chunkreplication"
+	"eddisonso.com/go-gfs/internal/chunkserver/stagedchunk"
+	"eddisonso.com/go-gfs/internal/chunkserver/chunkstagingtrackingservice"
 )
 
 type ReplicationPlane struct {
 	pb.UnimplementedReplicatorServer
 }
 
-func (rp *ReplicationPlane) Replicate(ctx context.Context, req *pb.ReplicationRequest) (*pb.ReplicationResponse, error) {
-    // Handle replication logic here
-    return &pb.ReplicationResponse{
-        Success: true,
-        Message: "replication accepted for " + req.ChunkHandle,
-    }, nil
-}
 
+func (rp *ReplicationPlane) Replicate(stream pb.Replicator_ReplicateServer) error {
+	var sc *stagedchunk.StagedChunk
+	var chunkHandle, opID string
+	var length uint64
+
+	for {
+		frame, err := stream.Recv()
+		if err == io.EOF {
+			// finalize / success response
+			msg := "replication accepted"
+			if chunkHandle != "" {
+				msg += " for " + chunkHandle
+			}
+			return stream.SendAndClose(&pb.ReplicationResponse{
+				Success: true,
+				Message: msg,
+			})
+		}
+
+		switch v := frame.GetKind().(type) {
+		case *pb.ReplicationFrame_Meta:
+			meta := v.Meta
+			chunkHandle = meta.GetChunkHandle()
+			opID = meta.GetOpId()
+			length = meta.GetLength()
+
+			sc = stagedchunk.NewStagedChunk(chunkHandle, opID, length)
+			chunkstagingtrackingservice.GetChunkStagingTrackingService().AddStagedChunk(sc)
+
+		case *pb.ReplicationFrame_Data:
+			if sc == nil {
+				return errors.New("DATA before META")
+			}
+
+			data := v.Data.GetData()
+			sc.Read(data)
+		default:
+			return errors.New("unknown frame kind")
+		}
+	}
+}
